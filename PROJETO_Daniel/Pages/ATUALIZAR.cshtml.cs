e385e545e6c5daa3bfc0acfb21b9246959c85e5d
User <danielramonalvesmatias2020@gmail.com>
2026-04-20 21:28:22 -0300

    ADD PRODUTOS E CRIEI NOVAS PAGINAS

---
+++ PROJETO_Daniel/Pages/ATUALIZAR.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PROJETO_Daniel.Pages
+{
+    public class ATUALIZARModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ PROJETO_Daniel/Pages/CADASTRAR.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PROJETO_Daniel.Pages
+{
+    public class CADASTRARModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
