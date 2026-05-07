bcb21f96d9858b6e950931abd7ce41427b04c268
User <danielramonalvesmatias2020@gmail.com>
2026-05-06 21:09:10 -0300

    cadastrar  e atualizações

--- PROJETO_Daniel/Pages/CADASTRAR.cshtml.cs
+++ PROJETO_Daniel/Pages/CADASTRAR.cshtml.cs
@@ -1,12 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PROJETO_Daniel.Context;
+using PROJETO_Daniel.Models;
 
 namespace PROJETO_Daniel.Pages
 {
     public class CADASTRARModel : PageModel
     {
+
+
+        private readonly AppDbContext _context;
+    
+
+
+        public CADASTRARModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+            public Produto Produto { get; set; }
+
+
         public void OnGet()
         {
         }
+
+        public void OnPost()
+        {
+
+            _context.Produtos.Add(Produto);
+            _context.SaveChanges();
+
+        }
+
+        
+
+
+
     }
+
 }
--- PROJETO_Daniel/Pages/Index.cshtml.cs
+++ PROJETO_Daniel/Pages/Index.cshtml.cs
@@ -1,13 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PROJETO_Daniel.Context;
+using PROJETO_Daniel.Models;
 
 namespace PROJETO_Daniel.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly AppDbContext _context;
+
+        public IndexModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Produto> Produtos { get; set; }
+
+
+
+
         public void OnGet()
         {
-
+            Produtos = _context.Produtos.ToList();
         }
     }
 }
